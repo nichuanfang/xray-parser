a987a65f10c9729c946ea0b73d45584f4dd62961
倪传方 <1290274972@qq.com>
2023-03-30 00:55:26 +0800

    3

--- xray-config-parser.py
+++ xray-config-parser.py
@@ -21,7 +21,6 @@ res: dict = json.load(config)
 config.close()
 routing: dict = res.get("routing")
 routingrules: list = routing.get("rules")
-
 # 每次都从更新第1条之后 以及倒数第二条之前 中间的数据
 
 myPolicy: str = url.read().decode()
