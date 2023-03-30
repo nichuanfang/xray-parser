332cbe47c3579375179161d8a46d29a25ad48754
chuanfang <f18326186224@gmail.com>
2023-03-30 13:09:12 +0800

    xray脚本生成 支持qx

--- build.py
+++ build.py
@@ -6,8 +6,6 @@ import json
 
 
 # 构建完成后 同步到qx的路由规则里
-policy_file_path = '/root/code/config-server/QX/MyPolicy.list'
-
 rules = []
 
 with open('routing_header.json') as routing_header:
@@ -25,13 +23,42 @@ for hrule in rh['rules']:
 
 for brule in rb['rules']:
     rules.append(brule)
+    strategy = rb['domainStrategy']
 
 for frule in rf['rules']:
     rules.append(frule)
+
+with open('dns.json') as dns_file:
+    dns = json.load(dns_file)
 
 # 拼到client中 
 with open('client.json') as client_file:
     client = json.load(client_file)
 
-client['routing'] = rules
-print(client)
+# 配置路由
+client['routing'] = {'domainStrategy': strategy,'rules': rules}
+# 配置dns
+client['dns'] = dns
+
+# 持久化
+json.dump(client,open('/root/assets/config/config.json','w+'))
+
+outbound_tag_map = {
+    'direct': 'DIRECT',
+    'block': 'REJECT',
+    'proxy': 'PROXY'
+}
+
+
+# 转化为QX的路由配置文件
+with open('/root/assets/config/QxPolicy.list','w+') as qx:
+    for rule in rb['rules']:
+        # 把规则写到list里
+        outboundTag:str = rule['outboundTag']
+        # HOST-SUFFIX,1688.com,DIRECT
+        domains:list = rule['domain']
+        for domain in domains:
+            if domain.startswith('full:'):
+                qx.writelines('HOST,'+(domain[5:]+',')+(outbound_tag_map[outboundTag.lower()])+'\n')
+            else:
+                qx.writelines('HOST-SUFFIX,'+(domain+',')+(outbound_tag_map[outboundTag.lower()])+'\n')
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# 生成dist文件
+python build.py
+# 重启nginx服务 向外部提供https服务
+docker restart nginx
