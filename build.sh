c53bd62c51e3b01458b668d494bd82380e3f0190
chuanfang <f18326186224@gmail.com>
2023-04-07 16:21:37 +0800

    1

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 docker restart nginx
 sleep 2s
 scp -P 60022 /root/assets/config/server/config.json root@154.202.60.190:/usr/local/etc/xray/config.json
-ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "systemctl restart xray"
+ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "systemctl restart xray" 
