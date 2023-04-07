5b15591e837428cc1a82272ec1925d5eed2e8e71
chuanfang <f18326186224@gmail.com>
2023-04-07 15:38:54 +0800

    增加延时

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 /usr/bin/python3 build.py
 # 重启nginx服务 向外部提供https服务
 docker restart nginx
-sleep 2s
+sleep 5s
 ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "/usr/local/etc/xray/restart_xray.sh"
