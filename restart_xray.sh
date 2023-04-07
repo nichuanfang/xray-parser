c40809293cdf25049e76396fb2997daa05a0bd21
chuanfang <f18326186224@gmail.com>
2023-04-07 15:06:27 +0800

    更新basic_auth版本的url

--- restart_xray.sh
+++ restart_xray.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://config.vencenter.cn/server/config.json -O /usr/local/etc/xray/config.json
+wget https://18326186224:0820nCf9270@config.vencenter.cn/server/config.json -O /usr/local/etc/xray/config.json
 systemctl restart xray.service
