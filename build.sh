ca8ef220b16e38a6f07182c4bacb09ffe58aae16
chuanfang <f18326186224@gmail.com>
2023-04-07 16:15:26 +0800

    重启xray脚本

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 /usr/bin/python3 build.py
 # 重启nginx服务 向外部提供https服务
 docker restart nginx
-sleep 3s
-ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 </root/code/xray-parser/restart_xray.sh
+sleep 2s
+scp -P 60022 /root/assets/config/server/config.json root@154.202.60.190:/usr/local/etc/xray/config.json
+ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "systemctl restart xray"
--- restart_xray.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget --user 18326186224 --password 0820nCf9270 https://config.vencenter.cn/server/config.json -O /usr/local/etc/xray/config.json
-systemctl restart xray.service
