882c3cadeb8de279a6289dae44ded6c4a3d51db7
chuanfang <f18326186224@gmail.com>
2023-04-07 15:52:41 +0800

    更改wget basic认证方式

--- restart_xray.sh
+++ restart_xray.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://config.vencenter.cn/server/config.json -O /usr/local/etc/xray/config.json
+wget --user 18326186224 --password 0820nCf9270 https://config.vencenter.cn/server/config.json -O /usr/local/etc/xray/config.json
 systemctl restart xray.service
