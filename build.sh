3febb24226e6ec04ba1e20f08f628974f7a97a3b
chuanfang <f18326186224@gmail.com>
2023-04-07 15:29:51 +0800

    延时启动

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 /usr/bin/python3 build.py
 # 重启nginx服务 向外部提供https服务
 docker restart nginx
-sleep 1s 
+sleep 2s
 ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 </root/code/xray-parser/restart_xray.sh
