c5f414377fca21c55be7dd7e62627d79b2b9fd9c
chuanfang <f18326186224@gmail.com>
2023-04-07 15:56:00 +0800

    delay

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 # 重启nginx服务 向外部提供https服务
 docker restart nginx
 sleep 3s
-ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 </root/code/xray-parser/restart_xray.sh
+ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "/usr/local/etc/xray/restart.sh"
