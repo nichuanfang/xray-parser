f97a6a906d1e4cfbbf445036f6dc87204f9b5907
倪传方 <1290274972@qq.com>
2023-04-25 20:16:47 +0800

    ✨ feat: 加入CICD
    消除敏感信息 私有仓库公开

--- build.py
+++ build.py
@@ -5,23 +5,44 @@
 import json
 import os
 
+# 读取环境变量
 
+# xray协议key
+XRAY_WINDOWS_KEY = os.environ.get('XRAY_WINDOWS_KEY','')
+# trojan协议key
+XRAY_TROJAN_KEY = os.environ.get('XRAY_TROJAN_KEY','')
 
-with open('/root/code/xray-parser/routing.json') as routing_file:
+if XRAY_WINDOWS_KEY == '':
+    raise RuntimeError('必须配置环境变量XRAY_WINDOWS_KEY!')
+
+if XRAY_TROJAN_KEY == '':
+    raise RuntimeError('必须配置环境变量XRAY_TROJAN_KEY!')
+
+XRAY_PARSER_PATH = '/xray-parser/'
+XRAY_PATH = '/xray/'
+
+with open(f'{XRAY_PARSER_PATH}routing.json') as routing_file:
     routing = json.load(routing_file)
 
-with open('/root/code/xray-parser/dns.json') as dns_file:
+with open(f'{XRAY_PARSER_PATH}dns.json') as dns_file:
     dns = json.load(dns_file)
 
 inbounds = []
 # 构建inbounds   文件夹路径 文件夹集合 文件集合
-for dir_path,dir_list,file_list in os.walk('/root/code/xray-parser/inbounds'):
+for dir_path,dir_list,file_list in os.walk(f'{XRAY_PARSER_PATH}inbounds'):
     for file in file_list:
         with open(dir_path+'/'+file) as inbound_file:
-            inbounds.append(json.load(inbound_file)) 
+            server_dict:dict = json.load(inbound_file)
+            if file[:-5]=='windows':
+                # 添加密钥
+                server_dict['settings']['clients'][0]['id'] = XRAY_WINDOWS_KEY
+            elif file[:-5]=='trojan':
+                # 添加密钥
+                server_dict['settings']['clients'][0]['password'] = XRAY_TROJAN_KEY
+            inbounds.append(server_dict) 
 
 # 拼到client中 
-with open('/root/code/xray-parser/server.json') as server_file:
+with open(f'{XRAY_PARSER_PATH}server.json') as server_file:
     server = json.load(server_file) 
 
 # 配置路由
@@ -32,4 +53,4 @@ server['dns'] = dns
 server['inbounds'] = inbounds
 
 # 持久化
-json.dump(server,open('/root/assets/config/server/config.json','w+'))
+json.dump(server,open(f'{XRAY_PATH}config.json','w+'))
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 生成dist文件
-/usr/local/bin/python /root/code/xray-parser/build.py
-# 重启nginx服务 向外部提供https服务
-docker restart nginx
-sleep 2s 
-scp -P 60022 /root/assets/config/server/config.json root@154.202.60.190:/usr/local/etc/xray/config.json
-ssh -p 60022 -o StrictHostKeyChecking=no -t root@154.202.60.190 "systemctl restart xray" 
-
