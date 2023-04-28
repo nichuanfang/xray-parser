478e00ef9463bc9738d1a9ba461a5eee097c10bb
倪传方 <1290274972@qq.com>
2023-04-28 15:05:52 +0800

    🐳 chore: 同步xray-parser到应用服务器

--- hook.sh
+++ hook.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -d "/root/xray-parser" ]; then
-    cd /root
+if [ ! -d "/root/code/xray-parser" ]; then
+    cd /root/code
     git clone https://github.com/nichuanfang/xray-parser.git
 else
-    cd /root/xray-parser
-    # 丢弃本地变更
+    cd /root/code/xray-parser
+    # 丢弃本地未提交变更
     git checkout .
     # 检出指定分支
     git checkout client
